1f2e2f0f0f4fc265228492e48ba7fbd470ced01e
dwkns <dwkns@me.com>
2024-10-14 17:41:30 +0100

    Add NJK files to tailwind detection

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{md,html,webc}"],
+  content: ["./src/**/*.{md,html,webc,njk}"],
   theme: {
     extend: {},
   },
