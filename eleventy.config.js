c007f7f96eddd17cef5e09078af0b5f641048374
dwkns <dwkns@me.com>
2025-03-11 17:03:39 +0000

    Use relative paths for tailwind

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,8 @@ import tailwindcss from 'eleventy-plugin-tailwindcss-4'
 
 export default (eleventyConfig) => {
   eleventyConfig.addPlugin(tailwindcss, {
-    input: './src/css/tailwind.css',
-    output:'./dist/styles.css'
+    input: 'css/tailwind.css',
+    output:'styles.css'
   });
 };
 
